258046d9cc28f742b2a47360c4f41a76d37b96a5
FutureProgrammerLi <52388490+FutureProgrammerLi@users.noreply.github.com>
2025-04-01 00:32:45 +0800

    record the process of fixing local search bug in 'search-plugin' file

--- docs/.vitepress/search-plugin.js
+++ docs/.vitepress/search-plugin.js
@@ -6,6 +6,18 @@
 
 // 1. 打包失败
 // import MarkJS from './lib/mark.js'
+// fix: 
+// 1. globally install mark.js 
+// 2. locate the reported file in node_modules/mark.js/src/vanilla.js
+// 3. install mark.js globally. 'npm install mark.js -g'
+// 4. modify from './lib/mark.js' to 'mark.js'  
+//   => another issue: fail to build on netlify, netlify will independently install deps of the project, resulting in the same issue in 'node_modules/mark.js/src/vanilla.js' on the server.
+// 5. use patch-package and postinstall-postinstall to change file on netlify server.
+// 5.1 'npm install patch-package postinstall-postinstall --save-dev'
+// 5.2 change 'node_modules/mark.js/src/vanilla.js' locally. (generate a package-lock.json before the next step, by running 'npm install' or 'npm install --package-lock-only'. 'cnpm install' will not generate package-lock.json)
+// 5.3 run 'npx patch-package mark.js' to generate patch file for mark.js.
+// 5.4 add a script in package.json: "scripts":'postinstall":"patch-package"'
+// 5.5 After doing 5.3,a file 'patches/mark.js+8.11.1.patch' will be generated.Upload it to github.And the problem will be solved.
 
 // defineConfig({
 //     themeConfig: {
